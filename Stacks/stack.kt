7ececd0abe48b2b189b5e3dbcfc66f399f0b3fb0
Ahmed Farouk <72602749+ahmed-faroukk@users.noreply.github.com>
2023-07-27 13:04:03 +0300

    Update stack.kt

--- Stacks/stack.kt
+++ Stacks/stack.kt
@@ -1,1 +1,62 @@
+import java.util.*
 
+fun main(args: Array<String>) {
+
+    val myStack = Stack<Int>()
+    if (myStack.isEmpty())
+        println("is empty now ")
+    myStack.push(1)
+    myStack.push(2)
+    myStack.push(3)
+    myStack.push(4)
+    myStack.pop()
+    println("Top is : " + myStack.top())
+    println("size = " + myStack.size())
+    println("print my stack")
+    printStack(myStack)
+    // output 
+    /*
+      is empty now 
+      Top is : 3
+      size = 3
+      print my stack
+      3
+      2
+      1
+    */
+
+}
+fun printStack(stack : Stack<Int>){
+   while (!stack.isEmpty()){
+       println(stack.top())
+       stack.pop()
+   }
+}
+class Stack<T>() {
+    private val stack = mutableListOf<T>()
+
+    fun push(element: T) {
+        stack.add(element)
+    }
+
+    fun pop() {
+        if (stack.isNotEmpty())
+            stack.removeAt(stack.size - 1)
+    }
+
+    fun top(): T? {
+        return if (stack.isNotEmpty())
+            stack[stack.size - 1]
+        else
+            null
+    }
+
+    fun isEmpty(): Boolean {
+        return stack.isEmpty()
+    }
+
+    fun size(): Int {
+        return stack.size
+    }
+
+}
