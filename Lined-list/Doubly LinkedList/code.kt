f54c5b544493b66b2e3d256a219711329ac61992
Ahmed Farouk <72602749+ahmed-faroukk@users.noreply.github.com>
2023-07-29 12:52:37 +0300

    Create code.kt

---
+++ Lined-list/Doubly LinkedList/code.kt
@@ -0,0 +1,58 @@
+
+fun main(args: Array<String>) {
+    val linkedList: SingleLinkedList<Int> = SingleLinkedList()
+    linkedList.insert(1)
+    linkedList.insert(2)
+    linkedList.insert(3)
+    linkedList.insert(4)
+    linkedList.display()
+    linkedList.displayFromBack()
+    //output
+    //1 <--> 2 <--> 3 <--> 4 <--> null
+    //4 <--> 3 <--> 2 <--> 1 <--> null
+    
+}
+
+class Node<T>(val data: T) {
+    var next: Node<T>? = null
+    var previous: Node<T>? = null
+}
+
+class SingleLinkedList<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+
+    fun insert(data: T) {
+        val newNode = Node(data)
+        if (head == null) {
+            head = newNode
+            tail = newNode
+        } else {
+            tail?.next = newNode
+            newNode.previous = tail
+            tail = newNode
+
+        }
+    }
+
+    fun display() {
+        var current = head
+        if (current == null)
+            println("your linked-list is Empty") else {
+            while (current != null) {
+                print("${current.data} <--> ")
+                current = current.next
+            }
+            println("null")
+        }
+    }
+
+    fun displayFromBack() {
+        var current = tail
+        while (current != null) {
+            print("${current.data} <--> ")
+            current = current.previous
+        }
+        println("null")
+    }
+}
